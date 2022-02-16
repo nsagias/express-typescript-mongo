087484b1763753193958b2cad8891e9ac7ea163d
Nick <nsagias@gmail.com>
2022-02-16 00:35:14 -0500

    impor dotenv

--- config/db.ts
+++ config/db.ts
@@ -1,15 +1,18 @@
-import mongoose, { Document, Model, model, Types, Schema, Query } from "mongoose";
+import * as dotenv from "dotenv";
+import mongoose, { Document, Model, model, Types, Schema, Query, ConnectOptions } from "mongoose"
 
+dotenv.config();
+const MONGO_URI: string = process.env.MONGO_URI as string ;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
+    const conn = await mongoose.connect(MONGO_URI, <ConnectOptions>{
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
+  } catch (error: any) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
