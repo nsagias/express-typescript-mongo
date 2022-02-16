26d6fb6e2fea279a9c15cf71ea1f6052228fecf2
Nick <nsagias@gmail.com>
2022-02-15 23:46:00 -0500

    remove starter code and update with code from server.js file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import express, { Request, Response} from "express";
 import cors from "cors";
 import helmet from "helmet";
-import products from "../data/products_db";
+import products_2 from "../data/products_2_db";
 import { IProduct, Product, Products } from "../products.models";
 
 const app = express();
@@ -14,53 +14,22 @@ dotenv.config();
 
 
 app.get('/', (req: Request, res: Response) => {
-  const message: {"welcome": string} = {"welcome": "Hello World!"};
-  res.json(message);
+  res.send('API is running...');
+
 });
 
 
 app.get('/products', (req: Request, res: Response) => {
-  let allProducts: Products;
-  try {
-    allProducts = [...products];
-    if (!allProducts) allProducts = {message: "undefind"};
-    if (!Array.isArray(allProducts)) allProducts = {message: "not and array"};
-    if (Array.isArray(allProducts) &&  allProducts.length <= 0) allProducts = {message: "zero length"};
-    
-    // res.json(allProducts);
-    
-  } catch (error: any) {
 
-    allProducts = {message: error.message};
-    console.error(allProducts);
-  };
-  res.status(200).json(allProducts);
+    res.json(products_2);
+
 
 });
 
 
 app.get('/products/:id', (req: Request, res: Response) => {
-  let productById: Product;
-  try {
-    productById = products.find((p) => p._id === req.params.id);
-    if (!productById) productById = {message: "undefind"};
-    if (productById !== productById as IProduct ) productById = {message: "undefind"};
-    return res.json(productById);
-
-  } catch (error: any) {
-
-    productById = {message: error.message};
-    console.error(productById);
-    return res.status(404).json(productById);
-   
-  };
-
-});
-
-
-app.get('/a-products', async (req: Request, res: Response) => {
-  const message: Products = await products;
-  res.json(message);
+  const product = products_2.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 
