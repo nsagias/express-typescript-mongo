9c86e90291b9e324be7a264f31c07a86d1e8fb4e
Nick <nsagias@gmail.com>
2022-02-20 23:43:00 -0500

    import products from product_seeder, create import data, WIP

--- seeder/mongo_seeder.ts
+++ seeder/mongo_seeder.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import users from "../data/users_db";
-import products_2 from "../data/products_2_db";
+import products from "../data/products_seeder";
 import User from "../models/userModel";
 import Product from "../models/productModel";
 import connectDB from "../config/db";
@@ -10,3 +10,16 @@ dotenv.config();
 
 connectDB();
 
+const importData = async () => {
+  try {
+    await User.deleteMany();
+    await Product.deleteMany();
+
+    await User.insertMany(users);
+    await Product.insertMany(products);
+  } catch (error) {
+    
+  };
+};
+
+ 
