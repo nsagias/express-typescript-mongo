fb25043e912774ec264f6b7cfdc8f62d8ce0b6b8
Nick <nsagias@gmail.com>
2022-02-20 22:57:41 -0500

    create user sample data

---
+++ data/users_db.ts
@@ -0,0 +1,22 @@
+
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: "xxxx",
+    isAdmin: true
+  },
+  {
+    name: "Second User",
+    email: "second@example.com",
+    password: "xxxx",
+    isAdmin: false
+  },
+  {
+    name: "Third User",
+    email: "third@example.com",
+    password: "xxxx",
+    isAdmin: false
+  }
+];
