4572201acbbcf70b811f2d3b6cb79905b9561744
Nick <nsagias@gmail.com>
2022-02-20 23:23:16 -0500

    create mongo seeder and import packages

--- seeder/mongo_seeder.ts
+++ seeder/mongo_seeder.ts
@@ -1,2 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import users from "../data/users_db";
+import products_2 from "../data/products_2_db";
+import User from "../models/userModel";
+import Product from "../models/productModel";
+import connectDB from "../config/db";
+
+dotenv.config();
+
+connectDB();
+
